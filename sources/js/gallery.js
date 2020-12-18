f799ae5f33575684f5ab495975e07f8a733e6a97
Stovenn <jonathan.sylvestre.pro@gmail.com>
2020-12-18 16:44:10 +0100

    quick refactor

--- sources/js/gallery.js
+++ sources/js/gallery.js
@@ -1,6 +1,8 @@
+
 (function(){
     const template = document.createElement('template');
 
+    //Template de la gallerie
     template.innerHTML = `
     <div id="slider">
         <img src="" id="currentImg">
@@ -10,6 +12,7 @@
     <div id ='bulletCtn'></div>
     
     `
+    //Feuille de style
     const style = `
         #slider {
             margin : auto;
@@ -64,7 +67,9 @@
             border-radius: 90px;
             background-color: #ccc;
         }
-        .selected:
+        .selected{
+            background-color: #000;
+        }
     `;
         class AppPhotoGalleryCmp extends HTMLElement{
             constructor(){
@@ -77,54 +82,81 @@
                 this.prevBtn = this.shadow.querySelector('#previousButton')
                 this.nextBtn = this.shadow.querySelector('#nextButton')
                 this.currentImg = this.shadow.querySelector('#currentImg')
-                
-            }
-
-            connectedCallback(){
-                const styleElem = document.createElement('style')
-                styleElem.textContent = style;
-                this.shadow.appendChild(styleElem)
-                const imgs = [
+                this.imgs = [
                     {title: "girl", src: "../img/girl.jpg", selected: false},
                     {title: "plant", src: "../img/plant.jpg", selected: false},
                     {title: "Scene", src: "../img/scene.jpg", selected: false}
                 ]
-                imgs.forEach(img =>{
-                    const bulletPoint = document.createElement('div')
-                    bulletPoint.className = 'bullet'
-                    bulletPoint.id = img.src
-                    this.bulletCtn.appendChild(bulletPoint)
-                    bulletPoint.addEventListener('click', this.changePic.bind(this))
-                })
+            }
+            connectedCallback(){
+                //On rattache la feuille de style
+                const styleElem = document.createElement('style')
+                styleElem.textContent = style;
+                this.shadow.appendChild(styleElem)
 
+                //Création d'un tableau contenant la source des images
+                
+
+                this.currentImg.src = this.imgs[0].src
+                this.imgs[0].selected = true
+
+                
+                //Génération des bullets points
+                this.refresh()
+
+                //Previous button event
                 this.prevBtn.addEventListener('click', ()=>{
-                    const index = imgs.findIndex(pic => pic.src == this.currentImg.attributes.src.nodeValue)
+                    const index = this.imgs.findIndex(pic => pic.src == this.currentImg.attributes.src.nodeValue)
 
                     if (index == 0){
                         return
                     }
-                    this.currentImg.src = imgs[index-1].src
+                    this.imgs[index].selected = false
+                    this.imgs[index-1].selected = true
+                    this.currentImg.src = this.imgs[index-1].src
+                    this.refresh()
                 })
                 
-
+                //Next button event
                 this.nextBtn.addEventListener('click', ()=>{
-                    const index = imgs.findIndex(pic => pic.src == this.currentImg.attributes.src.nodeValue)
+                    const index = this.imgs.findIndex(pic => pic.src == this.currentImg.attributes.src.nodeValue)
 
-                    if (index >= imgs.length - 1){
+                    if (index >= this.imgs.length - 1){
                         return
                     }
-                    this.currentImg.src = imgs[index+1].src
+                    this.imgs[index].selected = false
+                    this.imgs[index+1].selected = true
+                    this.currentImg.src = this.imgs[index+1].src
+                    this.refresh()
                 })
 
-                this.currentImg.src = imgs[0].src
-                imgs[0].selected = true
-                
                 console.log('connected')
             }
 
+            refresh(){
+                this.bulletCtn.innerHTML = ""
+                this.imgs.forEach(img => {
+                    const bulletPoint = document.createElement('div')
+                    bulletPoint.className = 'bullet'
+                    bulletPoint.id = img.src
+
+                    img.selected == true ? bulletPoint.classList.add('selected') : bulletPoint.classList.remove('selected')
+
+                    this.bulletCtn.appendChild(bulletPoint)
+                    bulletPoint.addEventListener('click', this.changePic.bind(this))
+                })
+            }
+
             changePic(e){
+                const currentIndex = this.imgs.findIndex(pic => pic.src == this.currentImg.attributes.src.nodeValue)
+                const targetIndex = this.imgs.findIndex(pic => pic.src == e.target.id)
+
                 this.currentImg.src = e.target.id
-                e.target.classList.add('selected')
+
+                this.imgs[currentIndex].selected = false
+                this.imgs[targetIndex].selected = true
+
+                this.refresh()
             }
         }
 
